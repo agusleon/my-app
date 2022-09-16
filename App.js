d14e8e0b6ace2574ef2bf7d254ce8e861918ec4a
agusleon <agleon@fi.uba.ar>
2022-09-15 21:25:33 -0300

    Main Screen and button component

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import Screen from './Screens/MainScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Screen/>
+    </PaperProvider>
   );
 }
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hey bitch!!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
---
+++ Screens/MainScreen.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import HelloButton from "../Components/HelloButton";
+
+const Screen = () => {
+    return (
+        <HelloButton/>
+    );
+}
+
+export default Screen;
